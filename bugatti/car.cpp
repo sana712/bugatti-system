000c13b43bfab4ec5eee11c885ec5758ab62b7a6
sana712 <byunsansa898@gmail.com>
2025-05-02 17:55:36 -0800

    car class

---
+++ bugatti/car.cpp
@@ -0,0 +1,24 @@
+﻿#include "car.h"
+Car::Car(std::string manu, std::string mod, std::string col, int yr, int maxSpd, int p)
+    : Vehicle(manu, mod, col, yr, maxSpd), Sellable(p) {
+}
+
+
+void Car::drive() const  {
+    std::cout << "The car is driving at a speed of " << maxSpeed << " km/h.\n";
+}
+
+
+void Car::display() const  {
+    std::cout << "Car Manufacturer: " << manufacturer << "\n"
+        << "Model: " << model << "\n"
+        << "Color: " << color << "\n"
+        << "Year: " << year << "\n"
+        << "Max Speed: " << maxSpeed << " km/h\n"
+        << "Price: $" << price << "\n";
+}
+
+// پیاده‌سازی تابع فروش
+void Car::sell()  {
+    std::cout << "The car has been sold for $" << price << ".\n";
+}
--- bugatti/car.h
+++ bugatti/car.h
@@ -1,1 +1,22 @@
-#pragma once
+﻿#ifndef CAR_H
+#define CAR_H
+
+#include "Vehicle.h"
+#include "Sellable.h"
+#include <iostream>
+
+class Car : public Vehicle, public Sellable {
+public:
+    Car(std::string manu, std::string mod, std::string col, int yr, int maxSpd, int p);
+
+   
+    void drive() const override;
+
+  
+    void display() const override;
+
+    // پیاده‌سازی تابع فروش
+    void sell() override;
+};
+
+#endif
