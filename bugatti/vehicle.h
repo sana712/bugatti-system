12f28e873a0a7e414a0c8faf69ae71a2a7123597
sana712 <byunsansa898@gmail.com>
2025-05-02 17:44:41 -0800

    adding vehicle class

---
+++ bugatti/vehicle.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include <iostream>
+#include <string>
+
+class Vehicle {
+protected:
+    std::string manufacturer;
+    std::string model;
+    std::string color;
+    int year;
+    int maxSpeed;
+
+public:
+    Vehicle(std::string manu, std::string mod, std::string col, int yr, int maxSpd)
+        : manufacturer(manu), model(mod), color(col), year(yr), maxSpeed(maxSpd) {
+    }
+
+    virtual void drive() const = 0;        // تابع انتزاعی
+    virtual void display() const = 0;      
+    virtual ~Vehicle() {}
+};
+
+#endif
